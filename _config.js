fb8a2bc601fbcce7afe32ad80ba43dbaf10aff16
Khatiti <ramseykhatiti@gmail.com>
2024-09-23 01:21:02 +0300

    update connection string to ramsey db

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    //production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    //development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    //test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+
+    production: 'mongodb+srv://ramseykhatiti:CRkGnocLCQzYQnWj@ramseycluster.hhyio.mongodb.net/?retryWrites=true&w=majority&appName=RamseyCluster',
+    development: 'mongodb+srv://ramseykhatiti:CRkGnocLCQzYQnWj@ramseycluster.hhyio.mongodb.net/?retryWrites=true&w=majority&appName=RamseyCluster',
+    test: 'mongodb+srv://ramseykhatiti:CRkGnocLCQzYQnWj@ramseycluster.hhyio.mongodb.net/?retryWrites=true&w=majority&appName=RamseyCluster',
 }
 module.exports = config;
