2dd5dbec6fd43194e7f0941f084cf788e2af3853
Khatiti <ramseykhatiti@gmail.com>
2024-09-23 18:57:09 +0300

    trigger webhook change

--- _config.js
+++ _config.js
@@ -5,7 +5,6 @@ config.mongoURI = {
     //production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     //development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     //test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-
     production: 'mongodb+srv://ramseykhatiti:CRkGnocLCQzYQnWj@ramseycluster.hhyio.mongodb.net/?retryWrites=true&w=majority&appName=RamseyCluster',
     development: 'mongodb+srv://ramseykhatiti:CRkGnocLCQzYQnWj@ramseycluster.hhyio.mongodb.net/?retryWrites=true&w=majority&appName=RamseyCluster',
     test: 'mongodb+srv://ramseykhatiti:CRkGnocLCQzYQnWj@ramseycluster.hhyio.mongodb.net/?retryWrites=true&w=majority&appName=RamseyCluster',
